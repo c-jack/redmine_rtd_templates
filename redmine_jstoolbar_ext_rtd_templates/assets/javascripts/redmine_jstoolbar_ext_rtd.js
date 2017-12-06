7b7b9019e35ebab4180741770bca82c61ed54238
c-jack <32272557+c-jack@users.noreply.github.com>
2017-12-06 11:33:13 +0000

    Add files via upload
    Added template file

--- redmine_jstoolbar_ext_rtd_templates/assets/javascripts/redmine_jstoolbar_ext_rtd.js
+++ redmine_jstoolbar_ext_rtd_templates/assets/javascripts/redmine_jstoolbar_ext_rtd.js
@@ -1,57 +1,12 @@
 (function () {
   if(typeof RedmineWikiToolbarExt === 'undefined') return false;
-
-/*************************
-Template options start 
-*************************/
-
-var templates_options = [
-        "Solution",
-        "test2"
-];
-
-var template_content = {
-    Solution: `*Solution QA Review Checklist*
-
-*Requirements and Solution*
-Have the requirements been properly understood and clearly stated on the ticket? *Yes - refer to comments/description/No*
-Has the nature of the high level approach been clearly articulated, with caveats/limitations identified as required? *Yes - refer to comments/description/No*
-
-*Sign-off*
-Has the customer signed off the approach identified? *Yes - refer to comment/No*
-Has the work involved to prepare the solution been estimated?  *Yes - refer to comment/No*
-Has the work involved to prepare the solution been approved by the customer?  *Yes - refer to comment/No*
-
-*Preparation*
-Is there a data set with README that provides a worked example of the solution? *Yes - link to data set README/No*
-* have the steps followed to prepare the data set been clearly documented? *Yes/No*
-* are the steps required to verify the solution using the data set?  *Yes/No*
-* has time to prepare the solution been booked appropriately, with customer approval for overruns *Yes/No*
-
-*Delivery*
-Have the implementation steps to deliver the solution in a customer environment been identified? *Yes - refer to comment/No*
-Is it clearly stated what software version(s) are required for delivery of this work?  *Yes - refer to comment/No*
-Have dependencies with other pieces of work (i.e. tickets) been clearly identified?  *Yes - refer to comment and associated tickets/No*
-Has the ticket been associated with the correct milestone for delivery?  *Yes/No*
-Have the steps required for the customer to test the solution been documented? *Yes - refer to comment/No*
-
-*Documentation*
-Does the solution have reusable elements that could be incorporated into a HowTo?  *Yes/No*
-Has the HowTo been created, or has a ticket been raised to create this HowTo?  *Yes - link to HowTo/No*`, 
-    test2: "testiertest"
-};
-
-
-
-/*************************
-Template options end 
-*************************/
-
+  var templateKeys = Object.keys(template_content);
   var buttons = [
     {
       title: 'rtd_templates', type: 'button', after: 'help',
       fn: { wiki: function (event) {
-        var templates_buttons = RedmineWikiToolbarExt.rtd_templatesSubMenuButtons.get(this, templates_options);
+        //var templates_buttons = RedmineWikiToolbarExt.rtd_templatesSubMenuButtons.get(this, templates_options);
+        var templates_buttons = RedmineWikiToolbarExt.rtd_templatesSubMenuButtons.get(this, templateKeys);
         RedmineWikiToolbarExt.SubMenu.open(this.toolbar, event.target, templates_buttons)
       }}
     }
---
+++ redmine_jstoolbar_ext_rtd_templates/assets/javascripts/redmine_jstoolbar_ext_rtd_templates.js
@@ -0,0 +1,16 @@
+/*************************
+Template options start 
+*************************/
+
+var template_content = {
+Template1: `template 1 content`,
+Template2: `template 2 content`,
+Template3: `template 3 content`,
+Template4: `template 4 content`
+};
+
+
+
+/*************************
+Template options end 
+*************************/
